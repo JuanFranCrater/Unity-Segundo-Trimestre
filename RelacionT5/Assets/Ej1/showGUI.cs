beec435e7ad34bf09c4fed7eb54abbac6d63e03c
JuanFranCrater <juanfbenitezlopez@gmail.com>
2018-01-12 09:10:37 +0100

    Empezada relacion, ejercicio 2 acabado pero se puede poner mas bonito

---
+++ RelacionT5/Assets/Ej1/showGUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showGUI : MonoBehaviour {
+
+	bool mostrar=false;
+	string textoMostrar="Soy el mensaje mostrado con GUI.Label";
+	void Start () {
+		
+	}
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			mostrar = !mostrar;
+		}
+	}
+
+	void mostratLabel ()
+	{
+		GUI.skin.label.fontSize = 25;
+		int anchoCaracter = 15;
+		float x = (float)Screen.width / 2 - textoMostrar.Length * anchoCaracter / 2;
+		float y = (float)Screen.height / 2;
+		float ancho = 500;
+		float alto = 35;
+		Rect rect = new Rect (x, y, ancho, alto);
+		GUI.color = Color.cyan;
+		GUI.Label (rect, textoMostrar);
+	}
+
+	void OnGUI()
+	{
+		if(mostrar)
+		mostratLabel ();
+	}
+}
