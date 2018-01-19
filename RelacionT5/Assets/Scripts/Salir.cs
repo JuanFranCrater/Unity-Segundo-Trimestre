5381aa82393ce0e5575914adfbc85610681f2a83
JuanFranCrater <juanfbenitezlopez@gmail.com>
2018-01-19 08:12:47 +0100

    Avanznado

---
+++ RelacionT5/Assets/Scripts/Salir.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Salir : MonoBehaviour {
+
+		private Rect windowRect = new Rect ((Screen.width)/2-110, (Screen.height)/2-25, 220, 50);
+		int idWindow=0;
+		string mensaje="¿Desea Salir?";
+
+		private bool show = false;
+
+		void Update()
+		{
+			if (Input.GetKeyDown (KeyCode.Escape)) {
+				show = true;
+			}
+		}
+		void OnGUI () 
+		{
+		if (show) {
+			Time.timeScale = 0;
+			DisableAllScripts ();
+			windowRect = GUI.Window (idWindow, windowRect, DialogWindow, mensaje);
+		}
+		}
+
+		void DialogWindow (int windowID)
+		{
+			float y = 20;
+		if(GUI.Button(new Rect(5,y, windowRect.width/2-10, 20), "Si"))
+			{
+				Application.Quit ();
+				show = false;
+			}
+
+		if(GUI.Button(new Rect(windowRect.width/2+5,y, windowRect.width /2-10, 20), "No"))
+			{
+				Time.timeScale = 1;
+				ActivateAllScripts ();
+				show = false;
+			}
+		}
+	void DisableAllScripts()
+	{
+		foreach (GameObject go in GameObject.FindGameObjectsWithTag("controladores")) {
+			MonoBehaviour[] scripts = go.GetComponents<MonoBehaviour> ();
+			foreach (MonoBehaviour script in scripts) {
+				script.enabled = false;
+			}
+		}
+	}
+	void ActivateAllScripts()
+	{
+		foreach (GameObject go in GameObject.FindGameObjectsWithTag("controladores")) {
+			MonoBehaviour[] scripts = go.GetComponents<MonoBehaviour> ();
+			foreach (MonoBehaviour script in scripts) {
+				script.enabled = true;
+			}
+		}
+	}
+	}
---
+++ Scripts/Assets/ScrollView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollView : MonoBehaviour {
+	
+	Vector2 posMostrarScrollView = new Vector2 (10, 10);
+	string contenido = "Este es el textodel contenido del ScrollView";
+	public Rect posMostrarArea = new Rect (25,25,120,120);
+	public Rect posMostrarContendido = new Rect (0,0,400,400);
+
+	void OnGUI () {
+		posMostrarScrollView=GUI.BeginScrollView (posMostrarArea, posMostrarScrollView, posMostrarContendido);
+		GUI.TextArea (new Rect (0, 0, 400, 400), contenido);
+		GUI.Button (new Rect (10, 20, 100, 30), "PUlsame");
+		GUI.EndScrollView ();
+	}
+
+}
