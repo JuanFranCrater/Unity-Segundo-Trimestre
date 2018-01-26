f40943a4d5a753e8589884f845a2731f4d9500ce
JuanFranCrater <juanfbenitezlopez@gmail.com>
2018-01-26 09:06:35 +0100

    Enlaces

---
+++ Scripts/Assets/Escena6-7/Change.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Change : MonoBehaviour {
+
+	int botonClicked = 0;
+	string[] listaDeBotones = { "Uno", "Dos", "Tres" };
+	string contenidoTexto = "Modificame...";
+
+	void OnGUI()
+	{
+		contenidoTexto = GUI.TextField (new Rect (10, 10, 300, 30), contenidoTexto,30);
+
+		if (GUI.changed) {
+			Debug.Log ("Ha cambido el texto");
+		}
+
+		/*
+		//Detectar el botón pulsado
+		botonClicked= GUI.Toolbar(new Rect(10,10,120,75),botonClicked,listaDeBotones);
+		if (GUI.changed) {
+			Debug.Log ("Has hecho un click en un boton");
+			switch (botonClicked) {
+
+			case 0:
+				Debug.Log ("Boton 0");
+				break;
+			case 1:
+				Debug.Log ("Boton 1");
+				break;
+			case 2:
+				Debug.Log ("Boton 2");
+				break;
+			}
+		}
+		*/
+
+
+	}
+}
---
+++ Scripts/Assets/Escena6-7/StyleGUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StyleGUI : MonoBehaviour {
+
+	public GUIStyle estilo;
+	public GUISkin piel;
+	//https://assetstore.unity.com/packages/2d/gui/necromancer-gui-366
+	//https://forum.unity.com/threads/custom-skins.7174/
+
+	void OnGUI()
+	{
+		//CambiarAspecto ();	
+		//StylePublic();
+		Piel();
+	}
+
+	void CambiarAspecto()
+	{
+		GUI.Label (new Rect (10, 10, 200, 50), "Soy un Label Camuflado a caja", "box");
+		GUI.Button (new Rect (10, 80, 180, 50), "Soy el boton camufaldo \na Toggle", "toggle");
+	}
+	void Piel()
+	{
+		GUI.skin = piel;
+		GUI.Button (new Rect (10, 80, 180, 50), "Soy un boton con estilo", "button");
+	}
+	void StylePublic()
+	{
+		
+	}
+}
