5550745cef78deff8bf433477639738039435d38
JuanFranCrater <juanfbenitezlopez@gmail.com>
2018-02-02 09:04:32 +0100

    Comunicacion entre Objetos

---
+++ Scripts/Assets/Escena11/ComunicacionEntreObjetos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComunicacionEntreObjetos : MonoBehaviour {
+	string[] valores;
+
+	void Start () {
+		//Comunicar mensajes de un objeto a otro o aun conjunto de elementos. 
+		//SendMessage(nombre del metodo(No tiene que ser publico)) -> Envia a un objeto 
+		//BroadcastMessage(nombre del metodo(No tiene que ser publico)) -> Envia a un conjunto de objetos
+		valores = new string[]{"Hola","Adios"};
+	}
+
+	void Update () {
+		//Lanzar mensaje
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			GameObject.FindGameObjectWithTag ("Receptor").SendMessage ("MostrarDatos",valores);
+		}
+	}
+}
---
+++ Scripts/Assets/Escena11/ReceptorMensaje.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceptorMensaje : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+
+	void MostrarDatos(string[] mensaje)
+	{
+		Debug.Log (mensaje[0]);
+		Debug.Log (mensaje [1]);
+	}
+}
