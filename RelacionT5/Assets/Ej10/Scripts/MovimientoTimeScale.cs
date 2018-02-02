ae4b0bbf0c4a9ad476cc3949aee5b1acb72327ab
JuanFranCrater <juanfbenitezlopez@gmail.com>
2018-02-02 09:14:55 +0100

    Ejercicio 10 terminado, falta poner todo bonito

--- RelacionT5/Assets/Ej10/Scripts/MovimientoTimeScale.cs
+++ RelacionT5/Assets/Ej10/Scripts/MovimientoTimeScale.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovimientoTimeScale : MonoBehaviour {
+	Vector3 posIniObj=new Vector3(0f,0.5f,0f);
+	Vector3 posActObj;
+	float posXAct;
+	float posYAct;
+	float poszAct;
+	float velocidad = 0.5f;
+	float minimoX = -4.5f;
+	float MaximoX = 4.5f;
+	float minimoZ= -4.5F;
+	float MaximoZ= 4.5f;
 
-	Rigidbody _rgb;
-	// Use this for initialization
-	void Start () {
-		_rgb = GetComponent<Rigidbody> ();
-
+	void Start()
+	{
+		posActObj = posIniObj;
 	}
-
 	void FixedUpdate()
 	{
-		_rgb.AddForce(new Vector3(Input.GetAxis ("Horizontal")*50,0,Input.GetAxis("Vertical")*50));
+		posXAct=transform.position.x+(Input.GetAxis("Horizontal")*velocidad*Time.timeScale);
+		poszAct = transform.position.z + (Input.GetAxis ("Vertical") * velocidad*Time.timeScale);
+		posActObj = new Vector3 (Mathf.Clamp (posXAct, minimoX, MaximoX), posActObj.y, Mathf.Clamp (poszAct,minimoZ,MaximoZ));
+		transform.position = posActObj;
+			
 	}
 }
